e4569de44a1ac8b24a4ea07e13863c322ff5d7fc
sanjaycit <sanjaysenthilkumar04@gmail.com>
2024-02-14 22:01:57 +0530

    cloned

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import mongoose  from 'mongoose'
+import userRouter from './routers/user.route.js';
 const app=express()
 dotenv.config();
 const mongoUri = process.env.MONGO;
@@ -14,3 +15,5 @@ const db = mongoose.connection;
 app.listen(3001,()=>{
     console.log("Your server is listening on port 3001");
 })
+
+app.use("/api/user",userRouter);
